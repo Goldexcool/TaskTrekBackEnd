2101a8f7d85cc3505b5c61e9028659532439209d
Goldexcool <ogunseitangold105@gmail.com>
2025-04-16 23:10:15 +0100

    fixed access token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -20,7 +20,7 @@ const generateAccessToken = (userId) => {
   return jwt.sign(
     { id: userId },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '1d' }
   );
 };
 
